078f38d111f9f0cccf28d93f2c101c96e73b5997
Krishna <u2krishna@gmail.com>
2023-09-19 12:16:50 -0400

    Added MeepMeep

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DCMotorTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DCMotorTest.java
@@ -2,16 +2,24 @@ package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
 
 @Autonomous
+
+
 public class DCMotorTest extends LinearOpMode {
+
+    DcMotor dcMotor;
 
     @Override
     public void runOpMode() throws InterruptedException {
+
+        dcMotor = hardwareMap.get(DcMotor.class, "dcMotor1");
+
         waitForStart();
 
-        telemetry.log().add("Hello");
-
+        telemetry.log().add("Hello, this is DCMotor test");
+        dcMotor.setPower(0.5);
         sleep(5000);
     }
 }
