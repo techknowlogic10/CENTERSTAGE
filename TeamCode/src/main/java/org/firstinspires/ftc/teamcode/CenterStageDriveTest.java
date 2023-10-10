d143d224e00bdba92784aa65d209a6fbd014cc74
Yashasvi3107 <omghappy07@gmail.com>
2023-10-09 20:22:18 -0400

    Test

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/CenterStageDriveTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/CenterStageDriveTest.java
@@ -5,13 +5,7 @@ import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
-import com.qualcomm.robotcore.hardware.DigitalChannel;
-import com.qualcomm.robotcore.hardware.DistanceSensor;
-import com.qualcomm.robotcore.hardware.Servo;
-
-import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
-import org.opencv.core.Mat;
-
+//test git
 
 @TeleOp
 public class CenterStageDriveTest extends OpMode {
